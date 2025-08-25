22efe8d2282539b53902d6c4bc6eb775588273da
kev6711 <kevin.heckenmeyer@gmail.com>
2025-08-25 13:37:05 +0200

    Ajout des fonctionnalités du formulaire : ajout du fichier login.js - redirection vers page d'accueil (+ sauvegarde du token) si connexion true et affichage message d'erreur si connexion false

---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,44 @@
+/* Récupération du formulaire*/
+const loginForm = document.querySelector(".login_form")
+
+/* Fonction pour afficher message d'erreur si email ou mot de passe incorrects*/
+function displayErrorMessage () {
+    const formTitle = document.querySelector("h2")
+
+    /* Permet de supprimer l'ancien message d'erreur si plusieurs tentatives de connexion infructueuses*/
+    const oldloginErrorMessage = document.querySelector(".login_Error")
+    if (oldloginErrorMessage) {
+        oldloginErrorMessage.remove()
+    }
+    
+    const loginErrorMessage = `
+        <p class="login_Error"> Combinaison Email et mot de passe incorrecte </p> 
+    `
+    formTitle.innerHTML += loginErrorMessage
+}
+
+/* Ajout EventListener sur le formulaire */
+loginForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const loginInformations = {
+        email : event.target.querySelector("[name=email]").value,
+        password : event.target.querySelector("[name=password]").value
+    }
+    const logInUsefulInformations = JSON.stringify(loginInformations);
+
+    const responseLogin = await fetch("http://localhost:5678/api/users/login", {
+        method : "POST",
+        headers : { "Content-Type": "application/json" },
+        body : logInUsefulInformations
+    })
+
+    const responseLoginData = await responseLogin.json()
+
+    if (responseLogin.ok) {
+        localStorage.setItem("token", responseLoginData.token)
+        window.location.href = "../../index.html"
+    } else {
+        displayErrorMessage()
+    }
+})
